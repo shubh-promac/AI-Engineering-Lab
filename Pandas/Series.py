b288b8a0549297f16451ca91171b5da8dc78c89e
shubh-promac <shubh.moondra@gmail.com>
2026-05-16 13:25:54 +0100

    Learned how to handle series in pandas

--- Pandas/Series.py
+++ Pandas/Series.py
@@ -2,12 +2,13 @@ import pandas as pd
 
 print(pd.__version__)
 
-data = [1.4, 1, 1]
+data = [1.4, 1, 1, 0.9, 0.6, 0.067]
 
 series = pd.Series(data) # Creates an 1 dimensional array
 print(series)
 
-series = pd.Series(data, index=['a', 'b', 'c']) # i can also label the index, 
+series = pd.Series(data, index=['a', 'b', 'c', 'd', 'e', 'f'])
+# You can also label the index, there has to be an index label for each item in the data list otherwise you will get an error
 print(series)
 
 # We can also locate values using the index labels
@@ -25,3 +26,14 @@ print(series)
 # Position-based indexer. Use this if you want to find the first item, regardless of what its label is.
 print(series.iloc[0]) # or if you want to find the second item, you can use iloc[1]
 
+# Filtering a series using a boolean expression
+print(series [series >= 1])
+
+calories = {"Day 1": 1750, "Day 2": 2100, "Day 3": 1700}
+series = pd.Series(calories)
+# Adding data to an existing value in a series
+series.loc["Day 3"] += 800
+print(series.loc[["Day 3"]])
+
+# Filtering a series using a boolean expression
+print(series[series > 2000])
