0ba7e8f55e3625028d9c25af28500b698fcf596f
shubh-promac <shubh.moondra@gmail.com>
2026-05-16 10:57:51 +0100

    panda series

--- Pandas/Series.py
+++ Pandas/Series.py
@@ -11,12 +11,17 @@ series = pd.Series(data, index=['a', 'b', 'c']) # i can also label the index,
 print(series)
 
 # We can also locate values using the index labels
+
+# Shorthand method to locate values using the index labels is to use the square brackets
 print(series['a']) # this will print 1.4
+
+# Clearer way to locate values using the index labels is to use the loc method
 print(series.loc['a']) # this will also print 1.4
+
+# Locating values using the index labels also allows us to change the value of an item
+series.loc["c"] = 200
+print(series)
 
 # Position-based indexer. Use this if you want to find the first item, regardless of what its label is.
 print(series.iloc[0]) # or if you want to find the second item, you can use iloc[1]
-data = [set([1, 2, 3]), set([4, 5, 6]), set([7, 8, 9])]
 
-df = pd.DataFrame(data, columns=['A', 'B', 'C'])
-print(df)
