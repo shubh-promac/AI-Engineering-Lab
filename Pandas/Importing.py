8d5f88733dbef69cf2bad780a01b9ce9963af385
shubh-promac <shubh.moondra@gmail.com>
2026-05-16 15:05:27 +0100

    Importing with csv and json

---
+++ Pandas/Importing.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv('Pandas/pokemondata.csv')
+print(df) # This data will be truncated first and last 5 rows are shown. To see all data, use df.to_string()
+print(df.head(2)) # can also use df.tail(2) to see last 2 rows
+print(df.loc[67])
+
+print(df.to_string())
+
+df = pd.read_json('Pandas/pokemondata.json')
+print(df)
